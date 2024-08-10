f47af41ca57b35184ebe529fc416d7dbb3a6cbf7
qouteall <qouteall@163.com>
2024-08-10 11:52:25 +0800

    WIP upgrade to 1.21

--- src/main/java/qouteall/imm_ptl/core/platform_specific/mixin/common/MixinPlayerManager_MA.java
+++ src/main/java/qouteall/imm_ptl/core/platform_specific/mixin/common/MixinPlayerManager_MA.java
@@ -2,6 +2,7 @@ package qouteall.imm_ptl.core.platform_specific.mixin.common;
 
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.players.PlayerList;
+import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,12 +13,11 @@ import qouteall.imm_ptl.core.chunk_loading.ImmPtlChunkTracking;
 @Mixin(PlayerList.class)
 public class MixinPlayerManager_MA {
     @Inject(
-        method = "Lnet/minecraft/server/players/PlayerList;respawn(Lnet/minecraft/server/level/ServerPlayer;Z)Lnet/minecraft/server/level/ServerPlayer;",
+        method = "respawn",
         at = @At("HEAD")
     )
     private void onPlayerRespawn(
-        ServerPlayer oldPlayer,
-        boolean bl,
+        ServerPlayer oldPlayer, boolean bl, Entity.RemovalReason removalReason,
         CallbackInfoReturnable<ServerPlayer> cir
     ) {
         ImmPtlChunkTracking.removePlayerFromChunkTrackersAndEntityTrackers(oldPlayer);
